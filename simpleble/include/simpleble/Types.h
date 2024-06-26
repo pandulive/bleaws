3961ee011b2b793c060684430b84f36969efadbd
pandulive <nmpandu29@gmail.com>
2024-06-26 19:41:30 +0530

    BLE scan, connect and write

---
+++ simpleble/include/simpleble/Types.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+#include <vector>
+
+namespace SimpleBLE {
+
+using BluetoothAddress = std::string;
+
+// IDEA: Extend BluetoothUUID to include a `uuid` function that
+// returns the same string, but provides a homogeneous interface.
+using BluetoothUUID = std::string;
+
+// IDEA: Extend ByteArray to be constructed by a vector of bytes
+// and pointers to uint8_t.
+using ByteArray = std::string;
+
+enum class OperatingSystem {
+    WINDOWS,
+    MACOS,
+    LINUX,
+};
+
+// TODO: Add to_string functions for all enums.
+enum BluetoothAddressType : int32_t { PUBLIC = 0, RANDOM = 1, UNSPECIFIED = 2 };
+
+}  // namespace SimpleBLE
